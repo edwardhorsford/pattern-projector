56796c817b689643faa25c61fa7c9fd8b4cb2143
courtneypattison <courtneyp@ttison.ca>
2025-11-25 14:10:40 -0500

    Fix pink screen on calibrate page

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -4,9 +4,12 @@ test.describe("Visual Regression Testing", () => {
   test(`Calibrate page`, async ({ page }) => {
     await page.goto("/en/calibrate");
     await expect(page.getByTestId("calibration-canvas")).toBeVisible();
+    await page.addStyleTag({
+      content:
+        '[data-testid="calibration-canvas"] { visibility: hidden !important; }',
+    });
     await expect(page).toHaveScreenshot(`calibrate.png`, {
       fullPage: true,
-      mask: [page.getByTestId("calibration-canvas")], // rendering of canvas is non-deterministic
     });
   });
 
